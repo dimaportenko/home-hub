46a8bcda6054733ae81caf4818b1ad2562e6d101
Dima Portenko <troublesohard@gmail.com>
2024-03-14 20:57:35 +0200

    add links config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,30 @@
+const links = [
+  {
+    name: "Movies",
+    url: "/videos/movies",
+  },
+  {
+    name: "Astro",
+    url: "/videos/astro",
+  },
+  {
+    name: "Yoga",
+    url: "/videos/yoga",
+  },
+];
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col p-10">
       <h1 className="text-xl">Home</h1>
       <div className="flex flex-1 flex-row flex-wrap gap-4 ">
-        <p>
-          <a className="underline" href="/videos/movies">
-            Movies
-          </a>
-        </p>
+        {links.map((link) => (
+          <p key={link.url}>
+            <a className="underline" href={link.url}>
+              {link.name}
+            </a>
+          </p>
+        ))}
       </div>
     </main>
   );
